39a0cea692d4b4188faad8ae9974769e20db7304
amazingdanny <blaskoff_2003@abv.bg>
2023-03-10 18:26:49 +0200

    added 5 hacks to js

--- learn_abt_hacks.js
+++ learn_abt_hacks.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", (event) =>{
-    
-});
+document.addEventListener("DOMContentLoaded", (even) =>{
+    const BAC = document.getElementById("BAC")
+    const Cr_fail = document.getElementById("Cr_fail")
+    const inject = document.getElementById("inj")
+    const Insec_design = document.getElementById("Insec_des")
+    const Sec_misconfig = document.getElementById("Sec_misconfig")
+
+
+
+    document.querySelectorAll(".buttons").forEach(element => {
+        console.log("ok")
+        element.onclick = () =>{
+            document.querySelectorAll(".divs").forEach(object =>{
+                console.log("ok ok")
+                console.log(object.id, element.dataset.showdiv)
+                if(object.id == element.dataset.showdiv)
+                {
+                    console.log("ok ok ok")
+                    object.classList.remove("hidden")
+                }
+                else{
+                    object.classList.add("hidden")
+                }
+            });
+        };
+    });
+})
