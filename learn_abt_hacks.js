ecd7476458965904e8e56f416476a66e5bd20cce
amazingdanny <blaskoff_2003@abv.bg>
2023-03-10 20:42:05 +0200

    9 ot 10te haka gotowi

--- learn_abt_hacks.js
+++ learn_abt_hacks.js
@@ -2,8 +2,13 @@ document.addEventListener("DOMContentLoaded", (even) =>{
     const BAC = document.getElementById("BAC")
     const Cr_fail = document.getElementById("Cr_fail")
     const inject = document.getElementById("inj")
-    const Insec_design = document.getElementById("Insec_des")
+    const Insec_design = document.getElementById("insec_des")
     const Sec_misconfig = document.getElementById("Sec_misconfig")
+    const SSRF = document.getElementById("SSRF")
+    const SLMF = document.getElementById("SLMF")
+    const IAF = document.getElementById("IAF")
+    const VOC = document.getElementById("VOC")
+
 
 
 
